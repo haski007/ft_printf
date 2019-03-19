360b40b155bd66efe5d84ecdc54e7d463467eea1
Pikaliuk DEMIAN <pdemian@e1r7p15.unit.ua>
2019-03-19 20:23:23 +0200

    Save again

--- src/main.c
+++ src/main.c
@@ -20,8 +20,8 @@ int         main(void)
     int     *b = &a;
     short int  num = 32767;
 
-    my_result = ft_printf("MY test |||||||%p ---- %c ----- %hd\n", b, 'O', num);
-    org_result = printf("Original|||||||%p ---- %lc ----- %hd\n", b, 'O', num);
+    my_result = ft_printf("MY test |||||||%hhp ---- %lc ----- %lld\n", b, 'O', num);
+    org_result = printf("Original|||||||%p ---- %c ----- %0d\n", b, 'O', num);
     printf("\n\nMy result - %d\n", my_result);
     printf("Original reuslt - %d\n", org_result);
     return (0);
