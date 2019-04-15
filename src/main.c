bd14e64626f0766a0bdf76824519f3920e563077
Pikaliuk DEMIAN <pdemian@e1r5p8.unit.ua>
2019-04-15 21:05:47 +0300

    at least

--- src/main.c
+++ src/main.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pdemian <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/10 12:55:48 by pdemian           #+#    #+#             */
-/*   Updated: 2019/04/05 20:08:47 by pdemian          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../includes/ft_printf.h"
 
 void        exit_func()
@@ -28,8 +16,8 @@ int         main(void)
     int     *b = &a;
     // long long double  chlen = -0;
     
-    my_result = ft_printf("%llx*\n", 3231231231);
-    org_result = printf("%llx\n", 3231231231);
+    my_result = ft_printf("%f*\n", -0.0);
+    org_result = printf("%f\n", -0.0);
     printf("\n\nMy result - %d\n", my_result);
     printf("Original result - %d\n", org_result);
     //   system("leaks ft_printf"); 
